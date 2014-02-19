4dd6fb0265014645c5b8093e0e621c98f7279bb2
Chris Watson <cjywatson@gmail.com>
2014-02-19 14:00:30 +1100

    deleted the filed and commited

--- gapminder.R
+++
@@ -1,6 +0,0 @@
-
-data <- read.csv("data/gapminder-FiveYearData.csv", stringsAsFactors=FALSE)
-
-## Woah, I'm changing everything in version control
-
-# changing something for version control again
